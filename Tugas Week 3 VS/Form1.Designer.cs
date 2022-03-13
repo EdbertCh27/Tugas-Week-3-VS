d2e0c7221b54e91dd2b2c9b9b8892cde3f2d5e07
EdbertCh27 <edbertch27@gmail.com>
2022-03-13 23:31:16 +0800

    Add project files.

---
+++ Tugas Week 3 VS/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Tugas_Week_3_VS
+{
+    partial class FormPengaturanWarna
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblINPUT = new System.Windows.Forms.Label();
+            this.LblOutput = new System.Windows.Forms.Label();
+            this.BtnProcess = new System.Windows.Forms.Button();
+            this.TBoxInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LblINPUT
+            // 
+            this.LblINPUT.AutoSize = true;
+            this.LblINPUT.Location = new System.Drawing.Point(36, 27);
+            this.LblINPUT.Name = "LblINPUT";
+            this.LblINPUT.Size = new System.Drawing.Size(57, 20);
+            this.LblINPUT.TabIndex = 0;
+            this.LblINPUT.Text = "INPUT :";
+            // 
+            // LblOutput
+            // 
+            this.LblOutput.AutoSize = true;
+            this.LblOutput.Location = new System.Drawing.Point(36, 157);
+            this.LblOutput.Name = "LblOutput";
+            this.LblOutput.Size = new System.Drawing.Size(58, 20);
+            this.LblOutput.TabIndex = 1;
+            this.LblOutput.Text = "[.............]";
+            // 
+            // BtnProcess
+            // 
+            this.BtnProcess.Location = new System.Drawing.Point(36, 103);
+            this.BtnProcess.Name = "BtnProcess";
+            this.BtnProcess.Size = new System.Drawing.Size(94, 29);
+            this.BtnProcess.TabIndex = 2;
+            this.BtnProcess.Text = "Process";
+            this.BtnProcess.UseVisualStyleBackColor = true;
+            this.BtnProcess.Click += new System.EventHandler(this.BtnProcess_Click);
+            // 
+            // TBoxInput
+            // 
+            this.TBoxInput.Location = new System.Drawing.Point(36, 50);
+            this.TBoxInput.Name = "TBoxInput";
+            this.TBoxInput.Size = new System.Drawing.Size(306, 27);
+            this.TBoxInput.TabIndex = 3;
+            // 
+            // FormPengaturanWarna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TBoxInput);
+            this.Controls.Add(this.BtnProcess);
+            this.Controls.Add(this.LblOutput);
+            this.Controls.Add(this.LblINPUT);
+            this.Name = "FormPengaturanWarna";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblINPUT;
+        private System.Windows.Forms.Label LblOutput;
+        private System.Windows.Forms.Button BtnProcess;
+        private System.Windows.Forms.TextBox TBoxInput;
+    }
+}
+
---
+++ Tugas Week 3 VS/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_Week_3_VS
+{
+    public partial class FormPengaturanWarna : Form
+    {
+        public FormPengaturanWarna()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnProcess_Click(object sender, EventArgs e)
+        {
+            if (TBoxInput.Text.ToUpper().IndexOf("ISI:") == 0)
+            {
+                LblOutput.Text = TBoxInput.Text.Remove(0,4);
+            }
+            else if (TBoxInput.Text.ToUpper().IndexOf("WARNA:") == 0)
+            {
+                if(TBoxInput.Text.ToUpper().IndexOf("DEFAULT") == 6)
+                {
+                    LblOutput.ForeColor = Color.Black;
+                }
+                if (TBoxInput.Text.ToUpper().IndexOf("MERAH") == 6)
+                {
+                    LblOutput.ForeColor = Color.Red;
+                }
+                if (TBoxInput.Text.ToUpper().IndexOf("HIJAU") == 6)
+                {
+                    LblOutput.ForeColor = Color.Green;
+                }
+                if (TBoxInput.Text.ToUpper().IndexOf("BIRU") == 6)
+                {
+                    LblOutput.ForeColor = Color.Blue;
+                }
+            }
+            else if (TBoxInput.Text.ToUpper().IndexOf("HIDE") == 0)
+            {
+                LblOutput.Visible = false;
+            }
+            else if (TBoxInput.Text.ToUpper().IndexOf("SHOWN") == 0)
+            {
+                LblOutput.Visible = true;
+            }
+
+            else if (TBoxInput.Text.ToUpper().IndexOf("BESARKAN") == 0)
+            {
+                LblOutput.Font = new Font(LblOutput.Font.FontFamily, LblOutput.Font.Size + 1F, LblOutput.Font.Style);
+            }
+            else if (TBoxInput.Text.ToUpper().IndexOf("KECILKAN") == 0)
+            {
+                LblOutput.Font = new Font(LblOutput.Font.FontFamily, LblOutput.Font.Size - 1F, LblOutput.Font.Style);
+            }
+            else if (TBoxInput.Text.ToUpper().IndexOf("RESTART") == 0)
+            {
+                LblOutput.Font = new Font(LblOutput.Font.FontFamily, 9F, LblOutput.Font.Style);
+            }
+
+            else
+            {
+                MessageBox.Show("Syntax Tidak Terdaftar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
